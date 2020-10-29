2a9061699a92b36e80bad7c3561ce5da91002de7
singamsricharan <singam.sricharan1603@gmail.com>
2020-10-29 17:00:46 +0530

    change sketch.js

--- Pig.js
+++ Pig.js
@@ -2,6 +2,21 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.Visiblity=255;
   }
-
-};
+  display(){
+    console.log(this.body.speed);
+    if(this.body.speed<3){
+      super.display();
+      
+    }
+    else{
+      push();
+      this.Visiblity = this.Visiblity -5;
+      World.remove(world,this.body);
+      tint(255,this.Visiblity);
+      image(this.image,this.body.position.x,this.body.position.y,50,50);
+      pop();
+    }
+  }
+}
--- sketch.js
+++ sketch.js
@@ -75,3 +75,9 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+
+function keyPressed(){
+    if (keyCode===32){
+        slingshot.attach(bird.body);
+    }
+}
