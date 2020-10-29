4c083bfde54654bf53102d6b87c7e29f818d9f06
singamsricharan <singam.sricharan1603@gmail.com>
2020-10-29 17:01:39 +0530

    change Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -13,11 +13,14 @@ class SlingShot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
+    attach(body){
+        this.sling.bodyA=body
+    }
 
     fly(){
         this.sling.bodyA = null;
     }
-
+    
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
